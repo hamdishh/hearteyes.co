851249791c9218009e0d901f483eb30740389cf5
Hamdi <hamdishh2003@gmail.com>
2024-07-30 21:04:13 +0100

    Updates

---
+++ precache-manifest.6843966eebb7fe8e4cae3795d6037916.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eccfd0824f6a48e714bfb9cb28930db",
+    "url": "/hearteyes.co/index.html"
+  },
+  {
+    "revision": "bdb40c53e48cb08849f2",
+    "url": "/hearteyes.co/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/hearteyes.co/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/hearteyes.co/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "bdb40c53e48cb08849f2",
+    "url": "/hearteyes.co/static/js/main.3caf29ec.chunk.js"
+  },
+  {
+    "revision": "dd3e67c36bdbbf7470ee",
+    "url": "/hearteyes.co/static/js/runtime~main.13deff76.js"
+  }
+]);
